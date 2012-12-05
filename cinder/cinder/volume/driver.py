81fb84b115ac1ab5ce4bd67f23c484ed5d7f61a1
yuanke wei <weiyuanke123@gmail.com>
2012-12-05 13:00:11 +0800

    Update cinder/cinder/volume/driver.py

--- cinder/cinder/volume/driver.py
+++ cinder/cinder/volume/driver.py
@@ -148,7 +148,7 @@ class VolumeDriver(object):
         # zero out old volumes to prevent data leaking between users
         # TODO(ja): reclaiming space should be done lazy and low priority
         #self._copy_volume('/dev/zero', self.local_path(volume), size_in_g)
-        self._copy_volume('/dev/zero', self.local_path(volume), 2)
+        self._copy_volume('/dev/zero', self.local_path(volume), 1)
         dev_path = self.local_path(volume)
         if os.path.exists(dev_path):
             self._try_execute('dmsetup', 'remove', '-f', dev_path,
