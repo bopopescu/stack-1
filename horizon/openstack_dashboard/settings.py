830dc5642a0aff895912474394eac7ff80afda56
yuanke wei <weiyuanke123@gmail.com>
2012-12-12 10:04:20 +0800

    翻译更新

--- horizon/openstack_dashboard/settings.py
+++ horizon/openstack_dashboard/settings.py
@@ -155,7 +155,7 @@ LANGUAGES = (
     ('zh-cn', gettext_noop('Simplified Chinese')),
     ('zh-tw', gettext_noop('Traditional Chinese')),
 )
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'zh-cn'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
