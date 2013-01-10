3bb8746ed2f5a7f26387884c9079d2462f932925
yuanke wei <weiyuanke123@gmail.com>
2013-01-10 16:28:25 +0800

    op log

--- nova/nova/api/AuditMiddleware.py
+++ nova/nova/api/AuditMiddleware.py
@@ -5,6 +5,7 @@ operation audit middleware
 from nova.openstack.common import log as logging
 from nova.openstack.common import local
 from nova import wsgi
+from nova import db
 import time
 import pdb
 
@@ -19,6 +20,7 @@ class AuditMiddleware(wsgi.Middleware):
         self._need_audit = req.method in self._audit_methods
         self._request = req
         self._requested_at = time.time()
+        self.context = getattr(local.store, 'context', None)
 
     def process_response(self, response):  
         if self._need_audit:
@@ -26,18 +28,16 @@ class AuditMiddleware(wsgi.Middleware):
         return response  
 
     def _store_log(self, response):
-        client_ip = self._request.headers.get('HOST')
-        roles = self._request.headers.get('X-Roles')
-        tenant = self._request.headers.get('X-Tenant-Name')
-        user = self._request.headers.get('X-User-Name')
-        request_time = self._requested_at
-        path_info = self._request.path_info
-        method = self._request.method
-        body = self._request.body
-        
+        value = []
+        value["client_ip"] = self._request.headers.get('HOST')
+        value["roles"] = self._request.headers.get('X-Roles')
+        value["tenant"] = self._request.headers.get('X-Tenant-Name')
+        value["user"] = self._request.headers.get('X-User-Name')
+        value["time_at"] = self._requested_at
+        value["path_info"] = self._request.path_info
+        value["method"] = self._request.method
+        value["body"] = self._request.body
+        db.operation_log_create(self.context, value)
 
-        print locals()
-        print "------------------------------------------------------"
-        print "------------------------------------------------------"
 
 
